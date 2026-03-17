88bd59be7b40bdf4d256567001df273840a97b20
Ameya-Date007 <ameyadate03@gmail.com>
2026-03-18 00:47:03 +0530

    Wrote bootloader v1

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,10 +1,17 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/scb.h>
 #include "core/system.h"
 #include "core/timer.h"
 
+#define BOOTLOADER_SIZE (0x8000U)
+
 #define LED_PORT (GPIOA)
 #define LED_PIN  (GPIO5)
+
+static void vector_setup(void){
+  SCB_VTOR = BOOTLOADER_SIZE;
+}
 
 static void gpio_setup(void) {
   rcc_periph_clock_enable(RCC_GPIOA);
@@ -20,6 +27,7 @@ static void gpio_setup(void) {
 */
 
 int main(void) {
+  vector_setup();
   system_setup();
   gpio_setup();
   timer_setup();
@@ -30,7 +38,7 @@ int main(void) {
   timer_set_pwm_duty_cycle(duty_cycle);
 
   while (1) {
-    if(system_get_ticks() - start_time >= 50){
+    if(system_get_ticks() - start_time >= 10){
       duty_cycle += 1.0f;
       if(duty_cycle > 100.0f){
         duty_cycle = 0.0f;
--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,3 +1,18 @@
+#include "common-defines.h"
+#include <libopencm3/stm32/f4/memorymap.h>
+#define BOOTLOADER_SIZE (0x8000U) // 32KB ----> 1000 0000 0000 0000
+#define MAIN_APP_START_ADDRESS (FLASH_BASE + BOOTLOADER_SIZE) // BASE ADDRESS OF FLASH MEM: 0x0800_0000
+
+static void jump_to_main(void){
+  typedef void (*void_fn)(void);
+  uint32_t * reset_vector_entry = (uint32_t *) (MAIN_APP_START_ADDRESS + 4U);
+  uint32_t * reset_vector = (uint32_t *) (*reset_vector_entry);
+  void_fn jump_fn = (void_fn) reset_vector;
+
+  jump_fn();
+}
+
 int main(void) {
+  jump_to_main();
   return 0;
 }
