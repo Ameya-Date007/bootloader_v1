cce21dd94b2efff21c9b3c1fbc5e1b72fc871876
Ameya-Date007 <ameyadate03@gmail.com>
2026-05-12 15:00:10 +0530

    firmware signing block added.

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -4,12 +4,16 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/cm3/vector.h>
 #include <libopencm3/cm3/scb.h>
+
+#include <string.h>
+
 #include "core/system.h"
 #include "bl-flash.h"
 #include "core/simple-timer.h"
 #include "core/uart.h"
 #include "comm.h"
 #include "core/firmware-info.h"
+#include "aes.h"
 #include "core/crc.h"
 
 #define BOOTLOADER_SIZE (0x8000U) // 32KB ----> 1000 0000 0000 0000
@@ -47,6 +51,12 @@ static uint8_t sync_seq[4] = {0};
 static simple_timer_t timer;
 static comm_packet_t temp_packet;
 
+static const uint8_t secret_key [AES_BLOCK_SIZE] = {
+  0x00, 0x01, 0x02, 0x03,
+  0x04, 0x05, 0x06, 0x07,
+  0x08, 0x09, 0x0a, 0x0b,
+  0x0c, 0x0d, 0x0e, 0x0f
+};
 
 static void gpio_setup(void){
   rcc_periph_clock_enable(RCC_GPIOA);
@@ -74,17 +84,71 @@ static void jump_to_main(void){
 
 }
 
+static void aes_cbc_mac_step(AES_Block_t aes_state, AES_Block_t prev_state, const AES_Block_t* key_schedule){
+  //CBC Chaining Operation
+  for (uint8_t i = 0; i < AES_BLOCK_SIZE; i++){
+    ((uint8_t*) aes_state)[i] ^= ((uint8_t*) prev_state)[i];
+  }
+
+  AES_EncryptBlock(aes_state, key_schedule);
+  memcpy(prev_state, aes_state, AES_BLOCK_SIZE);
+}
+
 static bool validate_firmware_image(void){
-  firmware_info_t* firmware_info = (firmware_info_t*)FWINFO_ADDRESS;
+  firmware_info_t* firmware_info = (firmware_info_t*) FWINFO_ADDRESS;
+  const uint8_t* signature = (uint8_t*) SIGNATURE_ADDRESS;
+
   if(firmware_info -> sentinel != FWINFO_SENTINEL){
     return false;
   }
   if(firmware_info -> device_id != DEVICE_ID){
     return false;
   }
-  const uint8_t* start_address = (uint8_t*) FWINFO_VALIDATE_FROM;
-  const uint32_t computed_crc = crc32(start_address, FWINFO_VALIDATE_LENGTH(firmware_info -> length));
-  return computed_crc == firmware_info -> crc32;
+
+  AES_Block_t round_keys[NUM_ROUND_KEYS_128];
+  AES_KeySchedule128(secret_key, round_keys);
+
+  AES_Block_t aes_state = {0};
+  AES_Block_t prev_state = {0};   //IV
+
+  uint8_t bytes_to_pad = 16 - (firmware_info->length % 16);
+  if(bytes_to_pad == 0){
+    bytes_to_pad = 16;
+  }
+
+  memcpy(prev_state, firmware_info, AES_BLOCK_SIZE);
+  aes_cbc_mac_step(aes_state, prev_state, round_keys);
+
+  uint32_t offset = 0;
+  while (offset < firmware_info->length){
+    if(offset == FWINFO_ADDRESS - MAIN_APP_START_ADDRESS){
+      offset += AES_BLOCK_SIZE * 2;
+      continue;
+    }
+
+    if(firmware_info -> length - offset > AES_BLOCK_SIZE){
+      //Regular case
+      memcpy(aes_state, (void*)(MAIN_APP_START_ADDRESS + offset), AES_BLOCK_SIZE);
+      aes_cbc_mac_step(aes_state, prev_state, round_keys);
+    } else {
+      if(bytes_to_pad == 16){
+        memcpy(aes_state, (void*)(MAIN_APP_START_ADDRESS + offset), AES_BLOCK_SIZE);
+        aes_cbc_mac_step(aes_state, prev_state, round_keys);
+
+        // Add whole padding
+        memset(aes_state, AES_BLOCK_SIZE, AES_BLOCK_SIZE);
+        aes_cbc_mac_step(aes_state, prev_state, round_keys);
+      } else {
+        memcpy(aes_state, (void*)(MAIN_APP_START_ADDRESS + offset), AES_BLOCK_SIZE - bytes_to_pad);
+        memset((void*)aes_state + (AES_BLOCK_SIZE - bytes_to_pad), bytes_to_pad, bytes_to_pad);
+        aes_cbc_mac_step(aes_state, prev_state, round_keys);
+      }
+    }
+    offset += AES_BLOCK_SIZE;
+  }
+
+  return memcmp(signature, aes_state, AES_BLOCK_SIZE) == 0;
+
 }
 
 static bool is_device_id_packet(const comm_packet_t * packet){
